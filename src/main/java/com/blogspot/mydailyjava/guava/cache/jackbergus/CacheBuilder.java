3ec33380227cc81d399a6f8527fe91475ed1256e
jackbergus <giacomo@openmailbox.org>
2015-03-08 15:43:37 +0100

    Added Tmp Stack and CacheBuilder

---
+++ src/main/java/com/blogspot/mydailyjava/guava/cache/jackbergus/CacheBuilder.java
@@ -0,0 +1,39 @@
+package com.blogspot.mydailyjava.guava.cache.jackbergus;
+
+import com.blogspot.mydailyjava.guava.cache.jackbergus.CacheMap;
+import com.blogspot.mydailyjava.guava.cache.overflow.FileSystemCacheBuilder;
+import com.blogspot.mydailyjava.guava.cache.overflow.FileSystemPersistingCache;
+import com.google.common.primitives.UnsignedLongs;
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class CacheBuilder {
+    private final static int TMP_CACHE_SIZE = 200;
+    
+    public static String getnerateTmpFileName() {
+        return new StringBuilder()
+                    .append("tmpCache")
+                    .append(UnsignedLongs.toString(new Date().getTime()))
+                    .append(UUID.randomUUID())
+                    .toString();
+        
+    }
+    
+    public static <K,V> CacheMap<K,V> createTmpCacheBuilder() {
+        try {
+            
+            //File f = File.createTempFile(getnerateTmpFileName(), "");
+            File f= java.nio.file.Files.createTempDirectory(getnerateTmpFileName()).toFile();
+            f.deleteOnExit();
+            return new CacheMap<>((FileSystemPersistingCache<K, V>) FileSystemCacheBuilder.<K,V>newBuilder().persistenceDirectory(f).maximumSize(TMP_CACHE_SIZE).softValues().build());
+        } catch (IOException ex) {
+            Logger.getLogger(CacheBuilder.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+}
---
+++ src/main/java/com/blogspot/mydailyjava/guava/cache/jackbergus/StackCache.java
@@ -0,0 +1,58 @@
+package com.blogspot.mydailyjava.guava.cache.jackbergus;
+
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+
+
+public class StackCache<T> extends Stack<T>{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 3644923435712459806L;
+	private final Map<T, Integer> countMap = CacheBuilder.createTmpCacheBuilder();
+    private final Map<Integer, Set<T>> stackMap = CacheBuilder.createTmpCacheBuilder();
+    private int maxCount = 0;
+    
+    @Override
+    public T push(T o) {
+        Integer c = countMap.get(o);
+        if (c == null) {
+            countMap.put(o, c = 1);
+        } else {
+            countMap.put(o, ++c);
+        }
+        Set<T> set = stackMap.get(c);
+        if (set == null)
+            stackMap.put(c, set = new LinkedHashSet<T>());
+        set.add(o);
+        if (c > maxCount)
+            maxCount = c;
+        return o;
+    }   
+    
+    @Override
+    public T pop() {
+        if (maxCount == 0)
+            return null;
+        Set<T> set = stackMap.get(maxCount);
+        T o = set.iterator().next();
+        set.remove(o);
+        if (maxCount == 1) {
+            countMap.remove(o);
+        }
+        if (set.size() == 0) {
+            stackMap.remove(maxCount);
+            --maxCount;
+        }
+        return o;
+    }   
+    
+    @Override
+    public T peek() {
+        if (maxCount == 0) 
+            return null;
+        return stackMap.get(maxCount).iterator().next();
+    }   
+}
